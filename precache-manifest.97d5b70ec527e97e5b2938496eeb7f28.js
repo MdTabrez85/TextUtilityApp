a602e1c03a9680a5e202793355017c6eb2e837bf
Tabrez <mdtabrez539@gmail.com>
2023-08-27 12:19:38 +0530

    Updates

---
+++ precache-manifest.97d5b70ec527e97e5b2938496eeb7f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce47f00391221cbeb9c0",
+    "url": "/static/css/main.afad1854.chunk.css"
+  },
+  {
+    "revision": "ce47f00391221cbeb9c0",
+    "url": "/static/js/main.8e1d55a4.chunk.js"
+  },
+  {
+    "revision": "1209b5989bd700f9325c",
+    "url": "/static/js/runtime~main.e8da6e7d.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "43d0d286182066179769f6470c500358",
+    "url": "/index.html"
+  }
+];
